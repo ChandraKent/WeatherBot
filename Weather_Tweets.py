29f8710002576f59945aa9fea7f6bd27627ab22d
ChandraKent <54602807+ChandraKent@users.noreply.github.com>
2019-10-30 21:15:19 -0400

    Test

--- Weather_Tweets.py
+++ Weather_Tweets.py
@@ -22,17 +22,6 @@ access_token = os.environ.get("access_token")
 access_token_secret = os.environ.get("access_token_secret")
 weather_api_key = os.environ.get("weather_api_key")
 
-
-# In[2]:
-
-
-## Twitter API Keys
-consumer_key = consumer_key
-consumer_secret = consumer_secret
-access_token = access_token
-access_token_secret = access_token_secret
-
-
 # In[3]:
 
 
